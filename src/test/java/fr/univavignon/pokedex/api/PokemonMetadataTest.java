7f3efd7ae8536dc4ac99cbef51d42c874e033291
Platoooon <nobili.swan@gmail.com>
2024-12-18 20:46:55 +0100

    Update Test

--- src/test/java/fr/univavignon/pokedex/api/PokemonMetadataTest.java
+++ src/test/java/fr/univavignon/pokedex/api/PokemonMetadataTest.java
@@ -7,10 +7,10 @@ public class PokemonMetadataTest {
 
     @Test
     public void testConstructorAndGetters() {
-        // Créer une instance de PokemonMetadata avec les données de l'Example1
+        // Create a PokemonMetadata instance with Example1 data
         PokemonMetadata pokemon = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
 
-        // Vérifier que les valeurs des attributs sont bien définies par le constructeur
+        // Check that the constructor correctly initializes the attribute values
         assertEquals(0, pokemon.getIndex());
         assertEquals("Bulbizarre", pokemon.getName());
         assertEquals(126, pokemon.getAttack());
@@ -20,31 +20,41 @@ public class PokemonMetadataTest {
 
     @Test
     public void testGetIndex() {
+        // Create a PokemonMetadata instance with Example1 data
         PokemonMetadata pokemon = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
+        // Check that the getIndex() method returns the correct value
         assertEquals(0, pokemon.getIndex());
     }
 
     @Test
     public void testGetName() {
+        // Create a PokemonMetadata instance with Example1 data
         PokemonMetadata pokemon = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
+        // Check that the getName() method returns the correct value
         assertEquals("Bulbizarre", pokemon.getName());
     }
 
     @Test
     public void testGetAttack() {
+        // Create a PokemonMetadata instance with Example1 data
         PokemonMetadata pokemon = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
+        // Check that the getAttack() method returns the correct value
         assertEquals(126, pokemon.getAttack());
     }
 
     @Test
     public void testGetDefense() {
+        // Create a PokemonMetadata instance with Example1 data
         PokemonMetadata pokemon = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
+        // Check that the getDefense() method returns the correct value
         assertEquals(126, pokemon.getDefense());
     }
 
     @Test
     public void testGetStamina() {
+        // Create a PokemonMetadata instance with Example1 data
         PokemonMetadata pokemon = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
+        // Check that the getStamina() method returns the correct value
         assertEquals(90, pokemon.getStamina());
     }
 }
--- src/test/java/fr/univavignon/pokedex/api/PokemonTest.java
+++ src/test/java/fr/univavignon/pokedex/api/PokemonTest.java
@@ -7,10 +7,10 @@ public class PokemonTest {
 
     @Test
     public void testConstructorAndGetters() {
-        // Créer une instance de Pokemon avec les données de l'Example1
+        // Create a Pokemon instance with Example1 data
         Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56.0);
 
-        // Vérifier que les valeurs des attributs sont bien définies par le constructeur
+        // Check that the constructor initializes the attributes correctly
         assertEquals(0, pokemon.getIndex());
         assertEquals("Bulbizarre", pokemon.getName());
         assertEquals(126, pokemon.getAttack());
@@ -25,31 +25,41 @@ public class PokemonTest {
 
     @Test
     public void testGetCp() {
+        // Create a Pokemon instance with Example1 data
         Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56.0);
+        // Check that the cp getter returns the correct value
         assertEquals(613, pokemon.getCp());
     }
 
     @Test
     public void testGetHp() {
+        // Create a Pokemon instance with Example1 data
         Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56.0);
+        // Check that the hp getter returns the correct value
         assertEquals(64, pokemon.getHp());
     }
 
     @Test
     public void testGetDust() {
+        // Create a Pokemon instance with Example1 data
         Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56.0);
+        // Check that the dust getter returns the correct value
         assertEquals(4000, pokemon.getDust());
     }
 
     @Test
     public void testGetCandy() {
+        // Create a Pokemon instance with Example1 data
         Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56.0);
+        // Check that the candy getter returns the correct value
         assertEquals(4, pokemon.getCandy());
     }
 
     @Test
     public void testGetIv() {
+        // Create a Pokemon instance with Example1 data
         Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56.0);
+        // Check that the iv getter returns the correct value
         assertEquals(56.0, pokemon.getIv());
     }
 }
